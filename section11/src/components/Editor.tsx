4ab41f2a847b1499d24fad9c12572c25a4b2bac9
nagul2 <hayani0000@gmail.com>
2026-02-08 20:20:29 +0900

    상태관리와 Props1

---
+++ section11/src/components/Editor.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+interface Props {
+  onclickAdd: (text: string) => void;
+}
+
+export default function Editor(props: Props) {
+  const [text, setText] = useState("");
+
+  const onChangeInput = (
+    e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+  ) => {
+    setText(e.target.value);
+  };
+
+  const onClickButton = () => {
+    props.onclickAdd(text);
+    setText("");
+  };
+
+  return (
+    <div>
+      <input value={text} onChange={onChangeInput}></input>
+      <button onClick={onClickButton}>Todo 추가</button>
+    </div>
+  );
+}
